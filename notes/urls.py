6ff45fe0cc4ca3074b4e0ad07c0501d08a73913d
vibikerski <vibikerski@gmail.com>
2024-03-28 17:49:14 +0100

    Minor fix

--- notes/urls.py
+++ notes/urls.py
@@ -4,5 +4,5 @@ from notes import views
 urlpatterns = [
     path('', views.NoteListView.as_view(), name='note-list'),
     path('<int:pk>/', views.NoteDetailView.as_view(), name='note'),
-    path('add', views.add_note, name='add-note'),
+    path('add/', views.add_note, name='add-note'),
 ]
