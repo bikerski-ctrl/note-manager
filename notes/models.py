f9ae1843ed4e03c5af607a042281dbdaf1b2a59f
vibikerski <vibikerski@gmail.com>
2024-03-28 17:44:55 +0100

    Adding notes

--- notes/models.py
+++ notes/models.py
@@ -1,6 +1,7 @@
 from django.db import models
+from datetime import date
 
 class Note(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
-    date = models.DateField()
+    date = models.DateField(default=date.today)
--- notes/urls.py
+++ notes/urls.py
@@ -4,4 +4,5 @@ from notes import views
 urlpatterns = [
     path('', views.NoteListView.as_view(), name='note-list'),
     path('<int:pk>/', views.NoteDetailView.as_view(), name='note'),
+    path('add', views.add_note, name='add-note'),
 ]
